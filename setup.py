48f09bd3bfd7ac640dce168e3dbfe662f44fa921
Nathaniel Case <Qalthos@gmail.com>
2012-08-31 12:22:33 -0400

    Add github url to setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     description='An amusing replacement error handler',
     author='Nathaniel Case',
     author_email='Qalthos@gmail.com',
-    url='',
+    url='https://github.com/Qalthos/errorcats',
     packages=find_packages(),
     package_data={'': ['*.mak']},
 )
