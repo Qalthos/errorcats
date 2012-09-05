cdb8fe430b94e8adff2c64a44523c8bd99c046ad
Nathaniel Case <Qalthos@gmail.com>
2012-09-05 16:13:18 -0400

    It finally works properly.

--- setup.py
+++ setup.py
@@ -4,10 +4,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='errorcats',
-    version='1.0',
+    version='1.0.2',
     description='An amusing replacement error handler',
     author='Nathaniel Case',
     author_email='Qalthos@gmail.com',
     url='https://github.com/Qalthos/errorcats',
     packages=find_packages(),
+    package_data={'errorcats': ['templates/*.mak']}
 )
